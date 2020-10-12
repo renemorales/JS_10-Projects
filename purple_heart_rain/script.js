37ee7b451a0fd34386b94f6f2f0ffe71e23cac62
Rene Morales <renemorales.developer@gmail.com>
2020-10-12 16:33:58 -0500

    deleted js code from preview project

--- purple_heart_rain/script.js
+++ purple_heart_rain/script.js
@@ -1,22 +1,3 @@
-const btn = document.getElementById("btn");
-const container = document.getElementById("container");
-
-btn.addEventListener('click', () => {
-  createNotification();
-});
-
-function createNotification() {
-  const notif = document.createElement('p');
-  notif.innerText = "Hello, Coders";
-
-  notif.classList.add('toast');
-
-  container.appendChild(notif);
-
-  setTimeout(() => {
-    notif.remove();
-  }, 3000);
-}
 
 
 
