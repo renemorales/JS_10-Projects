c50de36b48002ff5fc09ef7bad860fe6708d60f7
Rene Morales <renemorales.developer@gmail.com>
2021-05-17 14:22:12 -0500

    Fxed a minor bug and then added an alert for email validation.

--- capture-emails/js/app.js
+++ capture-emails/js/app.js
@@ -6,18 +6,47 @@ anonymous function is when a function does not have a name
 */
 window.onload = function() {
   //variables
+  let emailState = false;
   let emailModal = document.getElementsByClassName('email-modal')[0];
   let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+  let emailInput = document.getElementsByClassName('email-modal__input')[0];
+  let emailButton = document.getElementsByClassName('email-modal__button')[0];
+  let errorMessage = document.getElementsByClassName('email-modal__error-message')[0];
+  let errorForm = document.getElementsByClassName('email-modal__form-group')[0];
+
+  //validating email
+  function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email); //regex
+  }
+
+  emailButton.addEventListener('click', () => {
+    if(emailIsValid(emailInput.value)) {
+      console.log(emailInput.value);
+    }else{
+      errorForm.classList.add('email-modal__form-group--error');
+      errorMessage.classList.add('email-modal__error-message--active');
+    }
+  })
+
+  let showModal = () => {
+    if (emailState == false) {
+      // this type of event element are called  call-back
+      emailModal.classList.add('email-modal--visible');
+      emailState = true;
+    }
+
+  }
+  
   // removel modal when clicking the exit buttom
   closeModal.addEventListener('click', () => {
     emailModal.classList.remove('email-modal--visible');
   });
   // show modal when user is exting the page
-  document.addEventListener('mouseleave', () => {
-    // this type of event element are called  call-back
-      emailModal.classList.add('email-modal--visible');
-    console.log('exit');
-  });
+  document.body.addEventListener('mouseleave', () => {
+    showModal();
+    });
 
+
+console.log(document);
 
 }
